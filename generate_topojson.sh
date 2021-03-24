d61d667c0ae298f4752ee3ba5b6c0ba280de0002
Michele Locati <michele@locati.it>
2021-03-24 11:19:35 +0100

    Improve generate_topojson.sh
    - Don't require bash: let's make the script work in any posix shell
    - Exit on errors
    - Initialize IFS
    - Don't assume the script is launched in the project directory
    - Avoid old-fashion backticks to run sub-processes

--- generate_topojson.sh
+++ generate_topojson.sh
@@ -1,6 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-mapshaper\
+set -o errexit
+set -o nounset
+
+IFS='
+	 '
+
+CDPATH= cd -- "$(dirname -- "$0")"
+
+mapshaper \
     -i comuni.geojson -clean encoding=utf8 \
     -simplify 20% weighted \
     -o topojson/limits_IT_municipalities.topo.json bbox format=topojson
@@ -18,19 +26,17 @@ mapshaper \
     -o topojson/limits_IT_regions.topo.json bbox format=topojson target=regions \
     -o topojson/limits_IT_provinces.topo.json bbox format=topojson target=provinces
 
-for REG in `seq 1 20`
-do
-mapshaper\
-    -i topojson/limits_IT_municipalities.topo.json \
-    -filter reg_istat_code_num==$REG \
-    -o topojson/limits_R_${REG}_municipalities.topo.json bbox format=topojson
+for REG in $(seq 1 20); do
+    mapshaper \
+        -i topojson/limits_IT_municipalities.topo.json \
+        -filter reg_istat_code_num==$REG \
+        -o topojson/limits_R_${REG}_municipalities.topo.json bbox format=topojson
 done
 
-for PROV in `seq 1 111`
-do
-mapshaper\
-    -i topojson/limits_IT_municipalities.topo.json \
-    -filter prov_istat_code_num==$PROV \
-    -rename-layers municipalities target=1 \
-    -o topojson/limits_P_${PROV}_municipalities.topo.json bbox format=topojson
+for PROV in $(seq 1 111); do
+    mapshaper \
+        -i topojson/limits_IT_municipalities.topo.json \
+        -filter prov_istat_code_num==$PROV \
+        -rename-layers municipalities target=1 \
+        -o topojson/limits_P_${PROV}_municipalities.topo.json bbox format=topojson
 done
